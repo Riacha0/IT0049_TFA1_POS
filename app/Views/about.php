3eae3eda00860e0e43efc47c197f8ac7874af656
Riacha0 <ucanmailmigs@gmail.com>
2026-09-16 09:40:55 -0400

    Update about.php

--- app/Views/about.php
+++ app/Views/about.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>About the POS System</title>
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+   <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
     <nav>
